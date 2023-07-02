1c4ed7b86b729e01db045adeba19580a8c88256e
sally-kipo <saliyipiani@gmail.com>
2023-07-02 19:39:40 +0400

    change button clicked

--- class-app/src/Randomly.jsx
+++ class-app/src/Randomly.jsx
@@ -1,66 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import gryffindorImage from './Images/gryffindor.jpeg';
 import ravenclawImage from './Images/ravenclaw.webp';
 import slytherinImage from './Images/slytherin.png';
-import hufflepuffImage from './Images/hufflepuff.webp';
+import hufflepuffImage from './Images/hufflepuff.jpg';
 
-import {Container, GroupImage, GroupName, GroupWrapper} from './randomly.styled.jsx'
-
+import { Container, GroupImage, GroupName, GroupWrapper } from './randomly.styled.jsx';
 
 const StudentGroups = () => {
-    const [students, setStudents] = useState(['სალომე', 'ანი', 'მარი', 'თორნიკე', 'სოფია']);
-    const [groups, setGroups] = useState([[], [], []]);
-  
-    const assignGroupRandomly = () => {
-      if (students.length === 0) return;
-  
-      const randomStudentIndex = Math.floor(Math.random() * students.length);
-      const randomStudent = students[randomStudentIndex];
-      const randomGroupIndex = Math.floor(Math.random() * groups.length);
-  
-      const updatedGroups = [...groups];
-      updatedGroups[randomGroupIndex].push(randomStudent);
-  
-      const updatedStudents = students.filter((_, index) => index !== randomStudentIndex);
-  
-      setGroups(updatedGroups);
-      setStudents(updatedStudents);
-    };
-  
-    const renderGroups = () => {
-      const groupNames = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'];
-      const groupImages = [gryffindorImage, ravenclawImage, slytherinImage, hufflepuffImage];
-  
-      return groupNames.map((groupName, index) => (
-        <GroupWrapper key={index}>
-        
-          <GroupImage src={groupImages[index]} alt={groupName} />
-          <GroupName>{groupName}</GroupName>
-          <ul>
-            {groups[index] &&
-              groups[index].map((student, studentIndex) => (
-                <li key={studentIndex}>{student}</li>
-              ))}
-          </ul>
-        </GroupWrapper>
-      ));
-    };
-  
-    return (
-      <Container>
-        <div>
-          <ul>
-            {students.map((student, index) => (
-              <li key={index}>{student}</li>
-            ))}
-          </ul>
-          <button onClick={assignGroupRandomly} >
-            გამანაწილებელი ქუდი
-          </button>
-        </div>
-        <div>{renderGroups()}</div>
-      </Container>
-    );
+  const [students, setStudents] = useState(['სალომე', 'ანი', 'მარი', 'თორნიკე', 'სოფია']);
+  const [groups, setGroups] = useState([[], [], []]);
+
+  useEffect(() => {
+    if (students.length === 0) {
+      assignGroupRandomly();
+    }
+  }, [students.length]);
+
+  const assignGroupRandomly = () => {
+    if (students.length === 0) return;
+
+    const shuffledStudents = shuffleArray([...students]);
+    const updatedGroups = [[], [], []];
+
+    shuffledStudents.forEach((student, index) => {
+      const randomGroupIndex = index % updatedGroups.length;
+      updatedGroups[randomGroupIndex].push(student);
+    });
+
+    setGroups(updatedGroups);
+    setStudents([]);
   };
-  
-  export default StudentGroups;
+
+  const renderGroups = () => {
+    const groupNames = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff'];
+    const groupImages = [gryffindorImage, ravenclawImage, slytherinImage, hufflepuffImage];
+
+    return groupNames.map((groupName, index) => (
+      <GroupWrapper key={index}>
+        <GroupImage src={groupImages[index]} alt={groupName} />
+        <GroupName>{groupName}</GroupName>
+        <ul>
+          {groups[index] &&
+            groups[index].map((student, studentIndex) => (
+              <li key={studentIndex}>{student}</li>
+            ))}
+        </ul>
+      </GroupWrapper>
+    ));
+  };
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  return (
+    <Container>
+      <div>
+        <ul>
+          {students.map((student, index) => (
+            <li key={index}>{student}</li>
+          ))}
+        </ul>
+        {students.length > 0 && (
+          <button onClick={assignGroupRandomly}>გამანაწილებელი ქუდი</button>
+        )}
+      </div>
+      <div>{renderGroups()}</div>
+    </Container>
+  );
+};
+
+export default StudentGroups;
